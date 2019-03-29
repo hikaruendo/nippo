e88ed0eeaa946f220194a6ff37f573759e0c6ef3
hikaruendo <hikaru.endo@techacademy.jp>
2019-03-29 16:12:41 +0900

    Add Report update re

--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -17,7 +17,6 @@ import models.Report;
 import models.validators.ReportValidator;
 import utils.DBUtil;
 
-
 /**
  * Servlet implementation class ReportsUpdateServlet
  */
@@ -34,9 +33,9 @@ public class ReportsUpdateServlet extends HttpServlet {
     }
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = (String)request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
@@ -53,7 +52,7 @@ public class ReportsUpdateServlet extends HttpServlet {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("repoprt", r);
+                request.setAttribute("report", r);
                 request.setAttribute("errors", errors);
 
                 RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
@@ -68,9 +67,7 @@ public class ReportsUpdateServlet extends HttpServlet {
 
                 response.sendRedirect(request.getContextPath() + "/reports/index");
             }
-
         }
     }
-
 
 }
